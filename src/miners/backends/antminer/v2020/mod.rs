4ebd6e089ec06ae48537da5f1dc8f73c40ac69a7
Dan Nicolau <dnicolau@epicblockchain.io>
2026-03-11 14:43:26 -0400

    fix: use empty pointer to extract full miner conf response for pool passwords

--- src/miners/backends/antminer/v2020/mod.rs
+++ src/miners/backends/antminer/v2020/mod.rs
@@ -346,7 +346,7 @@ impl GetDataLocations for AntMinerV2020 {
                     WEB_MINER_CONF,
                     DataExtractor {
                         func: get_by_pointer,
-                        key: None,
+                        key: Some(""),
                         tag: Some("conf"),
                     },
                 ),
